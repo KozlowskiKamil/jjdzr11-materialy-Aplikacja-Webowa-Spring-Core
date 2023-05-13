a28cd27dc4edd0731c778b77e256008fb034ba2d
wojci <wojciech.rozwalka@mindconsulting.pl>
2023-05-13 16:31:11 +0200

    constructor injection

--- src/main/java/com/infoshareacademy/module/exercise01/CoffeeVendingMachine.java
+++ src/main/java/com/infoshareacademy/module/exercise01/CoffeeVendingMachine.java
@@ -1,13 +1,15 @@
 package com.infoshareacademy.module.exercise01;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CoffeeVendingMachine {
 
-    @Autowired
-    private CoffeeMaker coffeeMaker;
+    private final CoffeeMaker coffeeMaker;
+
+    public CoffeeVendingMachine(CoffeeMaker coffeeMaker) {
+        this.coffeeMaker = coffeeMaker;
+    }
 
     public void getCoffee() {
         coffeeMaker.getCoffee();
